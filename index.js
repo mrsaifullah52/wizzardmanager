0d9963da61e4dc5ba700a6c1380b459f69c0115e
Saif Ullah <mrsaifullah52.1@gmail.com>
2021-11-23 13:34:00 +0500

    Javascript and Bootstrap files has been setuped

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-
+import Path from 'path';
+const __dirname=Path.resolve();
 const app=express();
+
 app.use(cors());
 app.use(bodyParser.json());
-const PORT=process.env.PORT || 3000
+app.use(express.json());
+const PORT=process.env.PORT || 3333;
+
+app.set('view engine', 'ejs');
+app.set('views', "./views");
+app.use(express.static(__dirname + '/public'));
 
 app.get("/",(req, res)=>{
-  res.send("Homepage");
+  res.render('main',{
+    "name": "Saif",
+  })
 });
 
 app.listen(PORT, ()=>{
