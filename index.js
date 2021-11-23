bce7263a9a90c8c380d00a4722a396fcd3f76b40
Saif Ullah <mrsaifullah52.1@gmail.com>
2021-11-23 14:01:03 +0500

    views directory structure setuped

--- index.js
+++ index.js
@@ -2,24 +2,27 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import Path from 'path';
+import routes from './route/routes.js';
 const __dirname=Path.resolve();
 const app=express();
 
+// server configrations
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 const PORT=process.env.PORT || 3333;
-
 app.set('view engine', 'ejs');
 app.set('views', "./views");
 app.use(express.static(__dirname + '/public'));
 
-app.get("/",(req, res)=>{
-  res.render('main',{
-    "name": "Saif",
-  })
-});
 
+// general routes
+app.use("/", routes);
+
+
+
+
+// running server
 app.listen(PORT, ()=>{
   console.log("localhost:"+PORT);
 });
